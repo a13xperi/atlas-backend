cbd9cdce3fb90206d2f1e92c9999c88dc4a7a959
Alex Peri <aitkenassociates@gmail.com>
2026-04-15 01:31:42 +0200

    test(jest): anchor ts-jest config to repo root

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,14 @@
+const path = require("path");
+
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+  rootDir: __dirname,
   preset: "ts-jest",
   testEnvironment: "node",
   roots: ["<rootDir>/services/api/src"],
   testMatch: ["**/__tests__/**/*.test.ts"],
   transform: {
-    "^.+\\.tsx?$": ["ts-jest", { tsconfig: "tsconfig.test.json" }],
+    "^.+\\.tsx?$": ["ts-jest", { tsconfig: path.join(__dirname, "tsconfig.test.json") }],
   },
   moduleFileExtensions: ["ts", "tsx", "js", "json"],
   clearMocks: true,
